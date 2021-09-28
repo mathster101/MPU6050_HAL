e80b812902f0cb2026dfaeb28f05e4d6b6295012
Mathew P <mathewsimonpanicker@gmail.com>
2021-09-28 18:47:37 +0530

    fixed formatting

--- Src/MPU6050HAL.cpp
+++ Src/MPU6050HAL.cpp
@@ -8,7 +8,8 @@
 #include "MPU6050HAL.h"
 
 MPU6050_HAL::MPU6050_HAL(I2C_HandleTypeDef *i2c_object, GPIO_TypeDef *sclPort,
-		uint16_t sclPin, GPIO_TypeDef *sdaPort, uint16_t sdaPin) {
+		uint16_t sclPin, GPIO_TypeDef *sdaPort, uint16_t sdaPin)
+{
 	i2c_handle = i2c_object;
 	this->sclPort = sclPort;
 	this->sclPin = sclPin;
@@ -23,14 +24,16 @@ MPU6050_HAL::MPU6050_HAL(I2C_HandleTypeDef *i2c_object, GPIO_TypeDef *sclPort,
 /*
  * Write byte to a register
  */
-HAL_StatusTypeDef MPU6050_HAL::i2c_write_byte(uint8_t addr, uint8_t data_byte) {
+HAL_StatusTypeDef MPU6050_HAL::i2c_write_byte(uint8_t addr, uint8_t data_byte)
+{
 	HAL_StatusTypeDef result;
 	uint8_t *ptr;
 	ptr = &data_byte;
 	result = HAL_I2C_Mem_Write(i2c_handle, DEVICE_ADDR << 1, addr,
 	I2C_MEMADD_SIZE_8BIT, ptr, 1, //I2C_MEMADD_SIZE_8BIT must be used!!
 			HAL_MAX_DELAY);
-	if (result == HAL_BUSY) {
+	if (result == HAL_BUSY)
+	{
 		i2c_busy_resolve();
 		HAL_Delay(10);
 		result = HAL_I2C_Mem_Write(i2c_handle, DEVICE_ADDR << 1, addr,
@@ -43,7 +46,8 @@ HAL_StatusTypeDef MPU6050_HAL::i2c_write_byte(uint8_t addr, uint8_t data_byte) {
 /*
  * Read one byte from a register
  */
-uint8_t MPU6050_HAL::i2c_read_byte(uint8_t addr) {
+uint8_t MPU6050_HAL::i2c_read_byte(uint8_t addr)
+{
 	uint8_t buffer;
 	HAL_I2C_Mem_Read(i2c_handle, DEVICE_ADDR << 1, addr,
 	I2C_MEMADD_SIZE_8BIT, &buffer, 1,
@@ -55,7 +59,8 @@ uint8_t MPU6050_HAL::i2c_read_byte(uint8_t addr) {
  * Read multiple bytes from a starting register
  */
 HAL_StatusTypeDef MPU6050_HAL::i2c_read_bytes(uint8_t addr, uint8_t *buffer,
-		int num_bytes) {
+		int num_bytes)
+{
 	HAL_StatusTypeDef result;
 	result = HAL_I2C_Mem_Read(i2c_handle, DEVICE_ADDR << 1, addr,
 	I2C_MEMADD_SIZE_8BIT, buffer, num_bytes, HAL_MAX_DELAY);
@@ -65,7 +70,8 @@ HAL_StatusTypeDef MPU6050_HAL::i2c_read_bytes(uint8_t addr, uint8_t *buffer,
 /*
  * Sensor Initialization
  */
-HAL_StatusTypeDef MPU6050_HAL::initialize() {
+HAL_StatusTypeDef MPU6050_HAL::initialize()
+{
 	uint8_t reg;
 	double gyro_cali[3], gx_trim_temp = 0, gy_trim_temp = 0, gz_trim_temp = 0;
 	set_ranges(2, 3);
@@ -83,7 +89,8 @@ HAL_StatusTypeDef MPU6050_HAL::initialize() {
 		return HAL_ERROR;
 
 	int avg = 2000;
-	for (int i = 0; i < avg; i++) {
+	for (int i = 0; i < avg; i++)
+	{
 		get_gyro(gyro_cali);
 		gx_trim_temp += gyro_cali[0];
 		gy_trim_temp += gyro_cali[1];
@@ -99,14 +106,16 @@ HAL_StatusTypeDef MPU6050_HAL::initialize() {
 /*
  * Returns Chip ID
  */
-uint8_t MPU6050_HAL::whoami() {
+uint8_t MPU6050_HAL::whoami()
+{
 	return i2c_read_byte(WHO_AM_I);
 }
 
 /*
  * Set Gyro and Accelerometer full-scale ranges
  */
-HAL_StatusTypeDef MPU6050_HAL::set_ranges(int acc, int gyro) {
+HAL_StatusTypeDef MPU6050_HAL::set_ranges(int acc, int gyro)
+{
 
 	/*
 	 * accel_range = 2g, 4g, 8g, 16g
@@ -117,7 +126,8 @@ HAL_StatusTypeDef MPU6050_HAL::set_ranges(int acc, int gyro) {
 	accel_range = acc;
 	gyro_range = gyro;
 
-	switch (accel_range) {
+	switch (accel_range)
+	{
 	case 0:
 		acc_scale_factor = 16384;
 	case 1:
@@ -130,7 +140,8 @@ HAL_StatusTypeDef MPU6050_HAL::set_ranges(int acc, int gyro) {
 		acc_scale_factor = 16384;
 	}
 
-	switch (gyro_range) {
+	switch (gyro_range)
+	{
 	case 0:
 		gyro_scale_factor = 131;
 	case 1:
@@ -148,7 +159,8 @@ HAL_StatusTypeDef MPU6050_HAL::set_ranges(int acc, int gyro) {
 /*
  * Read Accelerometer data
  */
-HAL_StatusTypeDef MPU6050_HAL::get_accel(double *acc_buf) {
+HAL_StatusTypeDef MPU6050_HAL::get_accel(double *acc_buf)
+{
 	uint8_t buffer[6];
 	int16_t ax_raw, ay_raw, az_raw;
 	if (i2c_read_bytes(ACCEL_REG_START, (uint8_t*) buffer, 6) != HAL_OK)
@@ -168,7 +180,8 @@ HAL_StatusTypeDef MPU6050_HAL::get_accel(double *acc_buf) {
 /*
  * Read raw Gyro data
  */
-HAL_StatusTypeDef MPU6050_HAL::get_gyro(double *gyro_buf) {
+HAL_StatusTypeDef MPU6050_HAL::get_gyro(double *gyro_buf)
+{
 	uint8_t buffer[6];
 	int16_t gx_raw, gy_raw, gz_raw;
 	if (i2c_read_bytes(GYRO_REG_START, (uint8_t*) buffer, 6) != HAL_OK)
@@ -190,7 +203,8 @@ HAL_StatusTypeDef MPU6050_HAL::get_gyro(double *gyro_buf) {
 /*
  * Returns absolute value of acceleration in Gs
  */
-double MPU6050_HAL::accel_magnitude() {
+double MPU6050_HAL::accel_magnitude()
+{
 	double accel[3], temp;
 	if (get_accel(accel) != HAL_OK)
 		return -1;
@@ -203,7 +217,8 @@ double MPU6050_HAL::accel_magnitude() {
  */
 //pitch = 180 * atan2(accelX, sqrt(accelY*accelY + accelZ*accelZ))/PI;
 //roll = 180 * atan2(accelY, sqrt(accelX*accelX + accelZ*accelZ))/PI;
-HAL_StatusTypeDef MPU6050_HAL::get_pr_acc(double *angle_buf) {
+HAL_StatusTypeDef MPU6050_HAL::get_pr_acc(double *angle_buf)
+{
 	double mag = accel_magnitude();
 	angle_buf[0] = asin((float) ay_internal / mag) * -57.296;
 	angle_buf[1] = asin((float) ax_internal / mag) * 57.296;
@@ -216,11 +231,13 @@ HAL_StatusTypeDef MPU6050_HAL::get_pr_acc(double *angle_buf) {
  * gy for roll
  * gz for yaw
  */
-HAL_StatusTypeDef MPU6050_HAL::get_pry(double *angle_buf) {
+HAL_StatusTypeDef MPU6050_HAL::get_pry(double *angle_buf)
+{
 	HAL_StatusTypeDef result;
-	if (gyro_first_call) { // on first gyro call, get values from accel
+	if (gyro_first_call)
+	{ // on first gyro call, get values from accel
 		result = get_pr_acc(angle_buf);
-		if(result != HAL_OK)
+		if (result != HAL_OK)
 			return result;
 		angle_buf[2] = 0;
 		sys_tick = HAL_GetTick();
@@ -228,27 +245,34 @@ HAL_StatusTypeDef MPU6050_HAL::get_pry(double *angle_buf) {
 		roll_internal = angle_buf[1];
 		yaw_internal = 0; //angle_buf[2];
 		gyro_first_call = false;
-	} else {
+	}
+	else
+	{
 
 		uint32_t new_tick;
 		double gyro_buf[3];
 		double angles_acc[2];
 		result = get_gyro(gyro_buf);
-		if(result != HAL_OK)
+		if (result != HAL_OK)
 			return result;
 		new_tick = HAL_GetTick();
 		elapsed = (int32_t) (new_tick - sys_tick);
 		elapsed /= 1000; // time in seconds
-		angle_buf[0] = pitch_internal + (gyro_buf[0] * elapsed / gyro_scale_factor);
-		angle_buf[1] = roll_internal + (gyro_buf[1] * elapsed / gyro_scale_factor);
-		angle_buf[2] = yaw_internal + (gyro_buf[2] * elapsed / gyro_scale_factor);
+		angle_buf[0] = pitch_internal
+				+ (gyro_buf[0] * elapsed / gyro_scale_factor);
+		angle_buf[1] = roll_internal
+				+ (gyro_buf[1] * elapsed / gyro_scale_factor);
+		angle_buf[2] = yaw_internal
+				+ (gyro_buf[2] * elapsed / gyro_scale_factor);
 
 		//compensate for IMU yaw
-		angle_buf[0] += angle_buf[1] * sin(M_PI * (gyro_buf[2] * elapsed / gyro_scale_factor) / 180);
-		angle_buf[1] += angle_buf[0] * sin(M_PI * (gyro_buf[2] * elapsed / gyro_scale_factor) / 180);
+		angle_buf[0] += angle_buf[1]
+				* sin(M_PI * (gyro_buf[2] * elapsed / gyro_scale_factor) / 180);
+		angle_buf[1] += angle_buf[0]
+				* sin(M_PI * (gyro_buf[2] * elapsed / gyro_scale_factor) / 180);
 
 		result = get_pr_acc(angles_acc);
-		if(result != HAL_OK)
+		if (result != HAL_OK)
 			return result;
 		angle_buf[0] = alpha * angle_buf[0] + (1 - alpha) * angles_acc[0];
 		angle_buf[1] = alpha * angle_buf[1] + (1 - alpha) * angles_acc[1];
@@ -257,14 +281,15 @@ HAL_StatusTypeDef MPU6050_HAL::get_pry(double *angle_buf) {
 		yaw_internal = angle_buf[2];
 
 		bool angle_err = false;
-		for(int i = 0; i < 3 ; i++)
+		for (int i = 0; i < 3; i++)
 		{
-			if(angle_buf[i] > 100 || angle_buf[i] < -100){
+			if (angle_buf[i] > 100 || angle_buf[i] < -100)
+			{
 				angle_err = true;
 				break;
 			}
 		}
-		if(angle_err)
+		if (angle_err)
 			alpha = 0.1; // rapidly correct for angle out of bounds
 		else
 			alpha = 0.95;
@@ -277,14 +302,15 @@ HAL_StatusTypeDef MPU6050_HAL::get_pry(double *angle_buf) {
  * Read internal temperature
  */
 
-float MPU6050_HAL::get_temperature(){
+float MPU6050_HAL::get_temperature()
+{
 
 	uint8_t buffer[2];
 	int16_t raw_temp;
-	if(i2c_read_bytes(TEMP_REG_START, buffer, 2) != HAL_OK)
+	if (i2c_read_bytes(TEMP_REG_START, buffer, 2) != HAL_OK)
 		return -1;
-	raw_temp = buffer[0]<<8 | buffer[1];
-	return (double)(raw_temp / 340 + 36.5);
+	raw_temp = buffer[0] << 8 | buffer[1];
+	return (double) (raw_temp / 340 + 36.5);
 
 }
 
@@ -293,7 +319,8 @@ float MPU6050_HAL::get_temperature(){
  * Problem detailed here -> [https://www.st.com/resource/en/errata_sheet/es0136-stm32f100xc-stm32f100xd-and-stm32f100xe-highdensity-value-line-device-limitations-stmicroelectronics.pdf]
  * This function is a modification of this -> [https://electronics.stackexchange.com/a/281046]
  */
-void MPU6050_HAL::i2c_busy_resolve() {
+void MPU6050_HAL::i2c_busy_resolve()
+{
 	GPIO_InitTypeDef GPIO_InitStructure;
 	uint32_t gpio_alternate = GPIO_AF4_I2C1;
 	// 1. Clear PE bit.
@@ -314,11 +341,13 @@ void MPU6050_HAL::i2c_busy_resolve() {
 	HAL_GPIO_WritePin(sdaPort, sdaPin, GPIO_PIN_SET);
 
 	// 3. Check SCL and SDA High level in GPIOx_IDR.
-	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(sclPort, sclPin)) {
+	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(sclPort, sclPin))
+	{
 		asm("nop");
 	}
 
-	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(sdaPort, sdaPin)) {
+	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(sdaPort, sdaPin))
+	{
 		asm("nop");
 	}
 
@@ -326,7 +355,8 @@ void MPU6050_HAL::i2c_busy_resolve() {
 	HAL_GPIO_WritePin(sdaPort, sdaPin, GPIO_PIN_RESET);
 
 	//  5. Check SDA Low level in GPIOx_IDR.
-	while (GPIO_PIN_RESET != HAL_GPIO_ReadPin(sdaPort, sdaPin)) {
+	while (GPIO_PIN_RESET != HAL_GPIO_ReadPin(sdaPort, sdaPin))
+	{
 		asm("nop");
 	}
 
@@ -334,7 +364,8 @@ void MPU6050_HAL::i2c_busy_resolve() {
 	HAL_GPIO_WritePin(sclPort, sclPin, GPIO_PIN_RESET);
 
 	//  7. Check SCL Low level in GPIOx_IDR.
-	while (GPIO_PIN_RESET != HAL_GPIO_ReadPin(sclPort, sclPin)) {
+	while (GPIO_PIN_RESET != HAL_GPIO_ReadPin(sclPort, sclPin))
+	{
 		asm("nop");
 	}
 
@@ -342,7 +373,8 @@ void MPU6050_HAL::i2c_busy_resolve() {
 	HAL_GPIO_WritePin(sclPort, sclPin, GPIO_PIN_SET);
 
 	// 9. Check SCL High level in GPIOx_IDR.
-	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(sclPort, sclPin)) {
+	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(sclPort, sclPin))
+	{
 		asm("nop");
 	}
 
@@ -350,7 +382,8 @@ void MPU6050_HAL::i2c_busy_resolve() {
 	HAL_GPIO_WritePin(sdaPort, sdaPin, GPIO_PIN_SET);
 
 	// 11. Check SDA High level in GPIOx_IDR.
-	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(sdaPort, sdaPin)) {
+	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(sdaPort, sdaPin))
+	{
 		asm("nop");
 	}
 
